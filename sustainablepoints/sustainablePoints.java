82493aef46e1f107085943a71c6be004cf955da6
Byron Streit <streitb2020@gmail.com>
2024-10-19 19:03:49 -0400

    test case and updates

--- sustainablepoints/sustainablePoints.java
+++ sustainablepoints/sustainablePoints.java
@@ -5,5 +5,15 @@ public class sustainablePoints {
         Guest guest1 = new Guest("Byron", 126, 1, false,  0);
         guest1.nightStayed();
         
+        energyConsumption guest1Energy = new energyConsumption(1, 10);
+        double guest1EnergyScore = guest1Energy.calcEnergyScore();
+        
+        waterConsumption guest1Water = new waterConsumption(1, 10);
+        double guest1WaterScore = guest1Water.calcWaterScore();
+        
+        loyaltyPointConverter guest1Converter = new loyaltyPointConverter(guest1.loyaltyTier(0));
+        double guest1SustainScore = guest1Converter.calcSustainScore(guest1WaterScore, guest1EnergyScore, 0);
+        System.out.println(guest1Converter.convertToPoints(guest1SustainScore) + " loyalty points earned.");
+
     }
 }
