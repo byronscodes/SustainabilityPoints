8df1fb00fdccb555f7e700ba36a3cdf752983887
demianaattia <demianaa@vt.edu>
2024-10-19 13:53:27 -0400

    Update foodManagement.java

--- sustainablepoints/foodManagement.java
+++ sustainablepoints/foodManagement.java
@@ -1,1 +1,60 @@
 package sustainablepoints;
+import java.util.Scanner;
+
+public class foodManagement
+{
+    // String breakfast;
+    // String lunch;
+    // String dinner;
+    int breakfastQuant;
+    int lunchQuant;
+    int dinnerQuant;
+
+    
+
+    public foodManagement(int firstQuant, int secondQuant, int thirdQuant)
+    {
+        this.breakfastQuant = firstQuant;
+        this.lunchQuant = secondQuant;
+        this.dinnerQuant = thirdQuant;
+    }
+
+    public int breakfastCalc()
+    {
+        //
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the waste for breakfast");
+        int breakfastWaste = input.nextInt();
+
+        return breakfastWaste;
+    }
+
+    public int breakfastpointsCalc(int breakfastWaste)
+    {
+        if ()
+    }
+
+    public int lunchCalculation()
+    {
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the waste for lunch");
+        int lunchtWaste = input.nextInt();
+
+        return lunchtWaste;
+    }
+
+    public int dinnerCalculation()
+    {
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the waste for dinner");
+        int dinnerWaste = input.nextInt();
+
+        return dinnerWaste;
+    }
+}
