51e695586a55f7943b8f095a8b1f6e8e1d670775
Byron Streit <streitb2020@gmail.com>
2024-10-19 12:06:36 -0400

    basic implementations

--- sustainablepoints/energyConsumption.java
+++ sustainablepoints/energyConsumption.java
@@ -1,1 +1,17 @@
 package sustainablepoints;
+import java.util.Scanner;
+
+public class energyConsumption {
+    public double totalEnergyCon;
+    public double singleAvgEnergyCon;
+    public double averageEnergyCon;
+    public double energyConDifference;
+    Scanner scanner = new Scanner(System.in);
+
+    public energyConsumption(int guestsPerRoom, double singleAvgEnergyCon) {
+        this.singleAvgEnergyCon = singleAvgEnergyCon;
+        averageEnergyCon = singleAvgEnergyCon*guestsPerRoom;
+        totalEnergyCon = scanner.nextDouble();
+        energyConDifference = totalEnergyCon - averageEnergyCon;
+    }
+}
--- sustainablepoints/loyaltyPointConverter.java
+++ sustainablepoints/loyaltyPointConverter.java
@@ -1,1 +1,28 @@
 package sustainablepoints;
+
+public class loyaltyPointConverter {
+    public int loyaltyPointsEarned;
+    public String loyaltyTier;
+
+    public loyaltyPointConverter(String loyaltyTier) {
+        this.loyaltyTier = loyaltyTier;
+    }
+
+    public int convertToPoints(double waterConDifference, double energyConDifference, double foodConDifference) {
+        return 3;
+    }
+
+    public boolean addPointsToAccount(String name) {
+        switch (loyaltyTier) {
+            case "Base":
+                break;
+            case "Silver":
+                loyaltyPointsEarned *= 1.1;
+                break;
+            case "Gold":
+                loyaltyPointsEarned *= 1.2;
+                break;
+        }
+        return true;
+    }
+}
