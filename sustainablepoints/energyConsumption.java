47f92ba4bb212ca13dc438405b073180c4d8ec86
Byron Streit <streitb2020@gmail.com>
2024-10-19 13:59:36 -0400

    sustainability score added

--- sustainablepoints/energyConsumption.java
+++ sustainablepoints/energyConsumption.java
@@ -5,13 +5,18 @@ public class energyConsumption {
     public double totalEnergyCon;
     public double singleAvgEnergyCon;
     public double averageEnergyCon;
-    public double energyConDifference;
+    public double energyScore;
     Scanner scanner = new Scanner(System.in);
 
+    /**
+     * the constructor for this class
+     * @param guestsPerRoom guests per room
+     * @param singleAvgEnergyCon the average energy consumption of a single guest
+     */
     public energyConsumption(int guestsPerRoom, double singleAvgEnergyCon) {
         this.singleAvgEnergyCon = singleAvgEnergyCon;
-        averageEnergyCon = singleAvgEnergyCon*guestsPerRoom;
-        totalEnergyCon = scanner.nextDouble();
-        energyConDifference = (totalEnergyCon - averageEnergyCon) / averageEnergyCon;
+        this.averageEnergyCon = singleAvgEnergyCon*guestsPerRoom;
+        this.totalEnergyCon = scanner.nextDouble();
+        this.energyScore = 1 - ((totalEnergyCon - averageEnergyCon*0.8) / (averageEnergyCon*1.2));
     }
 }
